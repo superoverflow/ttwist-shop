76e0916c7b4fdbb3c1ada3880fd831c402a781d0
superoverflow <me@superoverflow.com>
2022-11-24 20:48:22 +0000

    Sample demo to get data from mysql

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function getData() {
+  const products = await prisma.product.findMany();
+  return products;
+}
+
+export default async function Page() {
+  const products = await getData();
+  return (
+    <>
+      {products.map((product) => (
+        <h1 key={product.id}>Hello, {product.name_gb} </h1>
+      ))}
+    </>
+  );
 }
