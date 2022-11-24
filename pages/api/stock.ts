2343eae5b4861baacea1413f8e9ede90ac4f4474
superoverflow <me@superoverflow.com>
2022-11-24 20:55:16 +0000

    Use of prisma from api

---
+++ pages/api/stock.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+type Record = {
+  name: string;
+  stock: number;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Record[]>
+) {
+  const prisma = new PrismaClient();
+  const products = await prisma.product.findMany();
+  const result = products.map((p) => ({
+    name: p.name_gb,
+    stock: p.stock,
+  }));
+  res.status(200).json(result);
+}
