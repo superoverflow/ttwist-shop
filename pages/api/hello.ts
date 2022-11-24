9ec98e38cfa283233b4cd4ad7e94d60c292b8450
superoverflow <me@superoverflow.com>
2022-11-24 20:49:08 +0000

    remove connection from api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import mysql from "mysql2"
 
 type Data = {
   name: string
@@ -9,8 +8,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const connection = mysql.createConnection(process.env.DATABASE_URL || "")
-  console.log('Connected to PlanetScale!')
-
   res.status(200).json({ name: 'John Doe' })
 }
