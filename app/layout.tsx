331b7b503b9560c65f034bdb6cef9293eac73833
superoverflow <me@superoverflow.com>
2022-12-14 18:12:37 +0000

    fixing build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import "./globals.css"
 
 export default function RootLayout({
-  title,
   children,
 }: {
-  title: string
   children: React.ReactNode
 }) {
   return (
     <html>
-      <head title={title} />
+      <head />
       <body>{children}</body>
     </html>
   )
